616389da1a5bc57fbed1bb65d4984f22e27d598b
bhavyamalla3 <bhavyamalla77@gmail.com>
2025-10-03 14:34:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,12 @@
 function showSection(sectionId){
-    // Hide welcome
     document.getElementById('welcome').style.display = 'none';
-    // Hide all sections first
     let sections = document.querySelectorAll('.section');
     sections.forEach(sec => sec.style.display = 'none');
-    // Show selected section
     document.getElementById(sectionId).style.display = 'block';
 }
 
 function backToWelcome(){
-    // Hide all sections
     let sections = document.querySelectorAll('.section');
     sections.forEach(sec => sec.style.display = 'none');
-    // Show welcome screen
     document.getElementById('welcome').style.display = 'flex';
 }
